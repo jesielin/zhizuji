7b93e2aacb8f325a305ce9e5bc4ef4c03317623b
shawn <happyshawn88@gmail.com>
2017-02-22 00:49:18 +0800

    init ########### 底部导航 bottom nav view 网络框架 retrofit rx okhttp icons

---
+++ app/src/main/java/zzj/com/zhizuji/exception/NetworkException.java
@@ -0,0 +1,11 @@
+package zzj.com.zhizuji.exception;
+
+/**
+ * Created by shawn on 2017-02-21.
+ */
+
+public class NetworkException extends Exception {
+    public NetworkException(String msg) {
+        super(msg);
+    }
+}
---
+++ app/src/main/java/zzj/com/zhizuji/network/entity/SocialTotal.java
@@ -0,0 +1,12 @@
+package zzj.com.zhizuji.network.entity;
+
+import java.util.List;
+
+/**
+ * Created by shawn on 2017-02-20.
+ */
+
+public class SocialTotal {
+    public int total;
+    public List<SocialItem> list;
+}
