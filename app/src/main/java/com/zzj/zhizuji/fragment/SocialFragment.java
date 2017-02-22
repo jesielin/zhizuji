94f84a25b1732e7cca971f564c52bbe34e86423f
shawn <happyshawn88@gmail.com>
2017-02-23 02:33:11 +0800

    评论过程  开头  太困了 睡觉

--- app/src/main/java/com/zzj/zhizuji/fragment/SocialFragment.java
+++ app/src/main/java/com/zzj/zhizuji/fragment/SocialFragment.java
@@ -9,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -39,6 +41,13 @@ public class SocialFragment extends BaseFragment implements SwipeRefreshLayout.O
     RecyclerView mRecyclerView;
     @BindView(R.id.refresh)
     SwipeRefreshLayout mSwipeRefreshLayout;
+    @BindView(R.id.edit_body)
+    View mCommentEditBody;
+    @BindView(R.id.et)
+    EditText etComment;
+    @BindView(R.id.send)
+    Button btnSend;
+
     private SocialAdapter mSocialAdapter;
     private Network mNetwork;
     private LinearLayoutManager mLayoutManager;
@@ -88,6 +97,7 @@ public class SocialFragment extends BaseFragment implements SwipeRefreshLayout.O
     }
 
 
+
     @Override
     public void onRefresh() {
         DebugLog.e("do on refresh");
@@ -117,6 +127,10 @@ public class SocialFragment extends BaseFragment implements SwipeRefreshLayout.O
                         mSwipeRefreshLayout.setRefreshing(false);
                     }
                 });
+    }
+
+    private void showCommentEditBody(){
+
     }
 
 
